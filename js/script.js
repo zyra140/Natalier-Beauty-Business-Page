ad2d95976eaa7bc89a00656219ca787797515531
zyra140 <hubert.zyra@gmail.com>
2025-10-07 22:02:00 +0200

    alfa 1.0

---
+++ js/script.js
@@ -0,0 +1,30 @@
+//COPYRIGHT YEAR
+
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+//STICKY NAVIGATION
+
+const sectionHeroEl = document.querySelector(".section-natelier");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
